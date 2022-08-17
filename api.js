a53acc9e8206fef80d6b7b27df9f5034ec4e78f7
enddl3224 <enddl3224@naver.com>
2022-08-17 14:38:00 +0900

    CREATE: api.js server

---
+++ api.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+const server = app.listen(3001, () => {
+    console.log('Start Server')
+});
+
+app.get('/api/users',async(req, res) => {
+    res.send('connect!!');
+});
